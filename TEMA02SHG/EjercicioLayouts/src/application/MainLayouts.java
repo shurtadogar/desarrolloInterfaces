77d41e253cbdc405951ecac434748d370e6ea1e0
Sebastian <32487946+shurtadogar@users.noreply.github.com>
2022-10-04 21:04:00 +0200

    Add files via upload

---
+++ TEMA02SHG/EjercicioLayouts/src/application/MainLayouts.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class MainLayouts extends Application {
+	private BorderPane rootLayout;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Carga el diseño del archivo FXML en la variable rootLayout
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainLayouts.class.getResource("MainLayouts.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Mostramos la escena del BorderPane de la variable rootLayot
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
